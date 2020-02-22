7c5ba425a03919077c876b31c854239e75670f9e
Felix Volny <github@volny.co>
2020-02-22 11:09:03 +0700

    use python3.7

--- Vagrantfile
+++ Vagrantfile
@@ -2,8 +2,7 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "hashicorp/bionic64"
-
+  config.vm.box = "generic/ubuntu1804"
   config.ssh.forward_agent = true
 
   # prisma
--- provision/python.sh
+++ provision/python.sh
@@ -1,8 +1,9 @@
 sudo apt-get install software-properties-common build-essential libssl-dev libffi-dev -q -y
-sudo apt install python3.7 python3.7-dev python3.7-venv -q -y
+sudo apt install python3.7 python3.7-dev python3.7-venv python3-pip -q -y
 python3.7 -m pip install --upgrade pip
 
 alias python=python3.7
-alias pip=python3.7 -m pip
+alias pip='python3.7 -m pip'
+
 # add pip stuff to path
 export PATH="/home/vagrant/.local/bin:$PATH"
