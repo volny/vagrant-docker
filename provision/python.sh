130582fbd9728b5ac47ae6950bf21cbe40fa7006
Felix Volny <github@volny.co>
2020-02-29 12:23:09 +0700

    drop down to python 3.6

--- provision/python.sh
+++ provision/python.sh
@@ -1,9 +1,8 @@
-sudo apt-get install software-properties-common build-essential libssl-dev libffi-dev -q -y
-sudo apt install python3.7 python3.7-dev python3.7-venv python3-pip -q -y
-python3.7 -m pip install --upgrade pip
+sudo apt-get install python3-dev python3-venv python3-pip -q -y
+python3 -m pip install --upgrade pip
 
-alias python=python3.7
-alias pip='python3.7 -m pip'
+echo "alias python=\"python3\"" >> .bashrc
+echo "alias pip=\"python3 -m pip\"" >> .bashrc
 
 # add pip stuff to path
 export PATH="/home/vagrant/.local/bin:$PATH"
